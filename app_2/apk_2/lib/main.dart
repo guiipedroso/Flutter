ed28b21f7156862bee6699fd224ef5fedaa32e9d
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-11-19 12:47:17 -0300

    Button

---
+++ app_2/apk_2/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MeuApp());
+
+class MeuApp extends StatelessWidget {
+
+  botaoAction(){
+    print("Clicou no botão.");
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Meu Segundo App'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Clique no botão abaixo'),
+              ElevatedButton(
+                child: Text('Click here'),
+                onPressed: botaoAction
+              )
+            ],
+          ), // Adicione um texto significativo aqui
+        ),
+      ),
+    );
+  }
+}
