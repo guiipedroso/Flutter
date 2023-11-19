093fd23e560c4594210073fe154c37226f13765b
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-11-19 13:35:30 -0300

    App 3

--- app_3/apk_3/lib/main.dart
+++ app_3/apk_3/lib/main.dart
@@ -1,52 +1,55 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MeuApp());
+botaoAction() {
+  print('clicado');
+}
 
 class MeuApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Meu Terceiro App'),
-        ),
-        body: Column(
-          children: <Widget> [
-            Exemplo(),
-            Exemplo()
-          ]  
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(title: Text('Meu Tserceiro App')),
+            body:Column(
+              children: [
+                Exemplo(
+                    title: 'Texto 1',
+                    onPress: () {
+                      print('Clicou no item 1');
+                    }),
+                Exemplo(
+                  title: 'Texto 2',
+                  onPress: () {
+                    print('Clicou no item 2');
+                  },
+                )
+              ],
+            )));
   }
 }
 
-class Exemplo extends StatelessWidget{
-  
-  botaoAction(){
-    print("Clicou no botão.");
-  }
-   
-   @override
-   Widget build(BuildContext){
+class Exemplo extends StatelessWidget {
+  Exemplo({this.title, required this.onPress});
 
+  var title;
+  VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      width:200,
-      height: 200,
-      margin: EdgeInsets.all(10),//espaçamento 
-      padding: EdgeInsets.all(10), //espaçamento interno
-      color: Colors.red,
+      width: 200,
+      color: Colors.yellow,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
       child: Column(
         children: [
-          Text('Exemplo'),
-          ElevatedButton(
-            child: Text('Click Here'),
-            onPressed: botaoAction()
+          Text(this.title),
+          TextButton(
+            onPressed: onPress,
+            child: Text("Clique aqui"),
           )
         ],
-      )
+      ),
     );
-   }
-
+  }
 }
